035bd7fe29a40e3da8dd1743ec587fd1aad060d7
MATSUBARA Nobutada <t12307043@gunma-u.ac.jp>
2017-12-05 11:47:19 +0900

    Refactor

--- src/Native/Highlight.js
+++ src/Native/Highlight.js
@@ -1,13 +1,12 @@
 var _matsubara0507$elmdeck$Native_Highlight = function() {
   function toHighlight(lang, code)
-  	{
-  		if (typeof hljs !== 'undefined' && lang && hljs.listLanguages().indexOf(lang) >= 0)
-  		{
-  			return hljs.highlight(lang, code, true).value;
-  		}
-
-  		return code;
-  	}
+  {
+    if (typeof hljs !== 'undefined' && lang && hljs.listLanguages().indexOf(lang) >= 0)
+    {
+      return hljs.highlight(lang, code, true).value;
+    }
+    return code;
+  }
 
   return {
     toHighlight: F2(toHighlight)
